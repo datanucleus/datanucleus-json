d556ca4858f8c24c82b00c7cd485d7a69893bc88
andyjefferson <andy@datanucleus.org>
2014-06-03 08:05:12 +0100

    [NUCJSON-52] Remove SchemaVerifierImpl

--- src/main/java/org/datanucleus/store/json/JsonStoreManager.java
+++ src/main/java/org/datanucleus/store/json/JsonStoreManager.java
@@ -104,7 +104,7 @@ public class JsonStoreManager extends AbstractStoreManager
                     StoreData sd = storeDataMgr.get(cmd.getFullClassName());
                     if (sd == null)
                     {
-                        CompleteClassTable table = new CompleteClassTable(this, cmd, new SchemaVerifierImpl(this, cmd, clr));
+                        CompleteClassTable table = new CompleteClassTable(this, cmd, null);
                         sd = newStoreData(cmd, clr);
                         sd.setTable(table);
                         registerStoreData(sd);
