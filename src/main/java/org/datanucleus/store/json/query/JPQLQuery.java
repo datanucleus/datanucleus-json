4e2eec2a7e60e22c321c94806be806545a87c580
andyjefferson <andy@datanucleus.org>
2016-02-26 15:09:23 +0000

    [NUCJSON-57] Support NUCCORE-1353

--- src/main/java/org/datanucleus/store/json/query/JPQLQuery.java
+++ src/main/java/org/datanucleus/store/json/query/JPQLQuery.java
@@ -110,12 +110,12 @@ public class JPQLQuery extends AbstractJPQLQuery
                 NucleusLogger.QUERY.debug(Localiser.msg("021074", "JPQL", "" + (System.currentTimeMillis() - startTime)));
             }
 
-            if (type == BULK_DELETE)
+            if (type == QueryType.BULK_DELETE)
             {
                 ec.deleteObjects(results.toArray());
                 return Long.valueOf(results.size());
             }
-            else if (type == BULK_UPDATE)
+            else if (type == QueryType.BULK_UPDATE)
             {
                 throw new NucleusException("Bulk Update is not yet supported");
             }
